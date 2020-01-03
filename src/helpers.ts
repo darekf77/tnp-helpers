8ce47114a5c1522225e250c927716420f3f8b122
Dariusz <darekf77@gmail.com>
2020-01-03 21:39:46 +0100

    fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,13 @@
 import * as _ from 'lodash';
+
 import { HelpersArrayObj } from './helpers-array-obj';
 import { HelpersMessages } from './helpers-messages';
 import { HelpersStringsRegexes } from './helpers-strings-regexes';
 import { HelpersEnvironment } from './helpers-environment';
 import { Helpers as HelperNg2Logger } from 'ng2-logger';
 //#region @backend
+import * as Task from 'task.js';
+import * as os from 'os';
 import * as child from 'child_process';
 import { Helpers as MorpiHelpers } from 'morphi';
 import { HelpersGit } from './helpers-git.backend';
@@ -131,6 +134,84 @@ export class HelpersTnp {
       if (value === 'false') env[k] = false;
     })
   }
+
+  //#region @backend
+  async  workerCalculateArray(
+    dataToSplit: any[],
+    operation: (dataChunk: any[], workerNumber?: number | undefined) => Promise<void>,
+    options?: {
+      maxesForWorkes?: { [workerMaxes: number]: number; };
+      workerLimit?: number;
+      globals?: any;
+    }
+  ) {
+
+    let { maxesForWorkes, workerLimit, globals } = options || {};
+    if (_.isUndefined(globals)) {
+      globals = {};
+    }
+    if (_.isUndefined(maxesForWorkes)) {
+      maxesForWorkes = {
+        0: 5, // no worker for 5 chunks
+        1: 10, // 1 worker up to 10 chunks
+        2: 15, // 2 workers up to 15 chunks,
+        3: 25, // 2 workers up to 15 chunks,
+        // above 15 chunks => {workerLimit}
+      }
+    }
+    if (_.isUndefined(workerLimit) || workerLimit === Infinity) {
+      workerLimit = (os.cpus().length - 1);
+    }
+    if (workerLimit <= 0) {
+      workerLimit = 0;
+    }
+
+    if ((_.isNumber(maxesForWorkes[0]) && maxesForWorkes[0] > 0 && dataToSplit.length <= maxesForWorkes[0]) ||
+      workerLimit === 0) {
+      return await operation(dataToSplit, void 0);
+    }
+    const workersNumber = Number(Object
+      .keys(maxesForWorkes)
+      .filter(key => key != '0')
+      .sort()
+      .reverse()
+      .find(key => maxesForWorkes[key] <= dataToSplit.length));
+    // console.log('workersNumber', workersNumber)
+    // console.log('_.isNumber(workersNumber)', _.isNumber(workersNumber))
+
+    let chunks: (any[])[] = [];
+    if (_.isNumber(workersNumber)) {
+      const splitEven = Math.floor(dataToSplit.length / workersNumber);
+      for (let workerIndex = 0; workerIndex <= workersNumber; workerIndex++) {
+        if (workerIndex === workersNumber) {
+          chunks[chunks.length - 1] = chunks[chunks.length - 1].concat(dataToSplit.slice(workerIndex * splitEven, dataToSplit.length))
+        } else {
+          chunks.push(dataToSplit.slice(workerIndex * splitEven, workerIndex * splitEven + splitEven));
+        }
+      }
+    }
+
+    const promises = [];
+    for (let n = 0; n < chunks.length; n++) {
+      ((chunks, n) => {
+        const dataChunk = chunks[n];
+        console.log(`worker ${n} ` + dataChunk.join(',\t'))
+        // console.log('pass to worker', Helpers)
+        let task = new Task({
+          globals: _.merge(globals, {
+            n,
+            dataChunk
+          }),
+          requires: {
+            request: 'request-promise',
+          }
+        });
+        promises.push(task.run(operation))
+      })(chunks, n);
+    }
+    return await Promise.all(promises);
+  }
+  //#endregion
 
   //#region @backend
   checkEnvironment = (deps?: Models.morphi.GlobalDependencies) => MorpiHelpers.checkEnvironment(deps)
