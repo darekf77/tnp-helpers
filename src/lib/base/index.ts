ef371d0363e46a306995f155232107c58c8bdc53
Dariusz Filipiak <darekf77@gmail.com>
2024-03-26 12:32:34 +0100

    $$$ update $$$

--- src/lib/base/index.ts
+++ src/lib/base/index.ts
@@ -2,6 +2,7 @@ export * from './base-project';
 export * from './base-project-resolver';
 export * from './base-feature-for-project';
 //#region @backend
+export * from './base-compiler-for-project.backend';
 export * from './base-command-line.backend';
 export * from './base-command-line-git.backend';
 //#endregion
