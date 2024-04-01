6f89ac188d85aae92015866cc5c76c060e77e365
Dariusz Filipiak <darekf77@gmail.com>
2024-04-01 10:10:47 +0200

    feat: base start config

--- src/lib/base/base-start-config.backend.ts
+++ src/lib/base/base-start-config.backend.ts
@@ -1,17 +1,93 @@
-// TODO
+//#region imports
+import { config } from "tnp-config/src";
+import { _ } from 'tnp-core/src'
+import { Helpers } from "../../index";
+import type { CommandLineFeature } from "../../index";
+import { BaseProject } from './base-project';
+import { CLASS } from "typescript-class-helpers/src";
+//#endregion
 
-// export class BaseStartConfig {
+export type BaseStartConfigOptions = Partial<BaseStartConfig>;
 
-//   constructor() {
+export class BaseStartConfig {
 
-//   }
+  public readonly argsv: string[] = process.argv;
+  public readonly shortArgsReplaceConfig: { [shortCommand in string]: string; } = {};
+  public readonly functionsOrClasses: { classOrFnName?: string; funcOrClass?: Function }[] = [];
+  public readonly ProjectClass: Partial<typeof BaseProject> = BaseProject;
+  constructor(options: BaseStartConfigOptions) {
 
-//   quickFixes() {
+    options = options ? options : {};
+    for (const key in options) {
+      if (Object.prototype.hasOwnProperty.call(options, key)) {
+        const value = options[key];
+        if (value) {
+          this[key] = value;
+        }
+      }
+    }
 
-//   }
 
-//   handleGLobalArguments() {
+    const commandArgIndex = 0;
+    const commandArg = this.argsv[commandArgIndex];
+    if (commandArg && !commandArg.startsWith('-')) {
+      const longerCommandVersion = this.shortArgsReplaceConfig[commandArg];
+      if (longerCommandVersion) {
+        this.argsv[commandArgIndex] = longerCommandVersion;
+      }
 
-//   }
+    }
 
-// }
+    let recognized = null;
+    let restOfArgs = [];
+    let methodNameToCall = undefined;
+    let methods = [];
+
+    for (const { classOrFnName, funcOrClass } of this.functionsOrClasses) {
+      if (_.isFunction(funcOrClass)) {
+        const classMethodsNames = CLASS.getMethodsNames(funcOrClass).filter(f => !f.startsWith('_'));
+        // if (vFnName === '') {
+        // console.log({ classMethodsNames, vFnName })
+        // }
+
+        const check = Helpers.cliTool.match({
+          functionOrClassName: classOrFnName,
+          restOfArgs: this.argsv,
+          argsReplacements: this.shortArgsReplaceConfig,
+          classMethodsNames,
+        });
+        if (check.isMatch || (this.argsv.length === 0 && classOrFnName === '')) {
+          recognized = funcOrClass;
+          restOfArgs = check.restOfArgs;
+          methodNameToCall = check.methodNameToCall;
+          methods = classMethodsNames;
+          // console.log('--- recognized command ---', { classOrFnName, classMethodsNames })
+        }
+      }
+    }
+
+
+    if (recognized) {
+      global?.spinner?.stop();
+      // console.log('--- recognized command ---', { recognized, methodNameToCall, restOfArgs, methods })
+      const obj: CommandLineFeature = new (recognized as any)(
+        Helpers.cliTool.globalArgumentsParserTnp(restOfArgs),
+        methodNameToCall,
+        this.ProjectClass.ins.nearestTo(process.cwd()),
+        process.cwd(),
+      );
+      process.stdin.resume();
+    } else {
+      Helpers.error('Command not recognized', false, true);
+    }
+  }
+
+  quickFixes() {
+
+  }
+
+  handleGLobalArguments() {
+
+  }
+
+}
--- src/lib/base/index.ts
+++ src/lib/base/index.ts
@@ -3,7 +3,7 @@ export * from './base-project-resolver';
 export * from './base-feature-for-project';
 export * from './commit-data';
 //#region @backend
-export * from './base-compiler-for-project.backend';
+export * from './base-start-config.backend';
 export * from './command-line-feature.backend';
 export * from './base-command-line.backend';
 //#endregion
