cf2ded5f950fd731e51411f82b1446d2465aee94
Dariusz Filipiak <darekf77@gmail.com>
2024-05-20 15:34:56 +0200

    $$$ update $$$

--- src/lib/base/core-project.ts
+++ src/lib/base/core-project.ts
@@ -1,36 +1,90 @@
-import { _, path } from 'tnp-core/src';
+import { _, path, crossPlatformPath, Helpers } from 'tnp-core/src';
+//#region @backend
+import { os } from 'tnp-core/src';
+// import { JSONFilePreset } from "lowdb/node";
+//#endregion
+import { Low } from "lowdb"
 
 export type CoreProjectEnvironment = {
   [envName: string]: {
     name: string;
-    npmTask?: 'start',
-    action?: () => void,
+    description?: string;
+    onlineLink?: string;
+    actionOrNpmTaskName?: string | (() => void),
   };
 };
 
+const defaultDb = {
+  projects: [],
+}
 
 
 export class CoreProject {
+  private static db: Low<typeof defaultDb>;
+  public static get isUnsingDB(): boolean {
+    return !!this.db;
+  }
+  static async useDB(appName: string): Promise<Low<typeof defaultDb>> {
+    //#region @backendFunc
+    // const userFolder = crossPlatformPath([os.homedir(), `./firedev/apps/${appName}`]);
+    // try {
+    //   Helpers.mkdirp(userFolder);
+    // } catch (error) { }
+    // const dbLocation = crossPlatformPath([userFolder, 'db.json']);
+    // if (!Helpers.exists(dbLocation)) {
+    //   this.db = await JSONFilePreset(dbLocation, defaultDb);
+    // }
+    // // @LAST this for base-project
+    // return this.db;
+    //#endregion
+  }
+
+  db() {
+    return CoreProject.useDB(_.kebabCase(this.url));
+  }
+
   static from(options: Omit<CoreProject, 'name' | 'url' | 'branch'>) {
     return _.merge(new (CoreProject as any)(), _.cloneDeep(options));
   }
+
+  provider: string = 'firedev';
   color?: string;
   description?: string;
   /**
-   * url for git repo
+   * ssh url for git repo
    */
   urlSSH?: string;
+  /**
+   * https url for git repo
+   */
   urlHttp?: string;
+  /**
+   * main branches - first is default
+   */
   branches: string[];
-  get branch() {
-    return _.first(this.branches);
-  }
+  /**
+   * project environments
+   */
   environments: CoreProjectEnvironment;
+
+  /**
+   * core porject name
+   */
   get name() {
     return path.basename(this.url).replace(/\.git$/, '');
   }
 
-  get url() {
-    return this.urlHttp ? this.urlHttp : this.urlSSH;
+  /**
+   * main (default for reset) branch
+   */
+  get branch() {
+    return _.first(this.branches);
+  }
+
+  /**
+   * url for git repo
+   */
+  get url(): string {
+    return (this.urlHttp ? this.urlHttp : this.urlSSH) || '';
   }
 }
