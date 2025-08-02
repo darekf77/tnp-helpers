699d02867c3adcf5c9089a341cade24838f53151
Dariusz Filipiak <darekf77@gmail.com>
2025-08-02 16:27:31 +0200

    release: v19.0.39

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,7 +1,7 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
-export const BUILD_FRAMEWORK_CLI_NAME = 'taon';
+export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.tnp-helpers';
 export const PROJECT_NPM_NAME = 'tnp-helpers';
-export const CURRENT_PACKAGE_VERSION = '19.0.38';
+export const CURRENT_PACKAGE_VERSION = '19.0.39';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
