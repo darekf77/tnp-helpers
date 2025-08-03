423e25de7297dfa51aea7e3172822dd4b38367e1
Dariusz Filipiak <darekf77@gmail.com>
2025-08-03 21:26:27 +0200

    release: v19.0.41

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,7 +1,8 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.tnp-helpers';
+export const BUILD_BASE_HREF = '';
 export const PROJECT_NPM_NAME = 'tnp-helpers';
-export const CURRENT_PACKAGE_VERSION = '19.0.40';
+export const CURRENT_PACKAGE_VERSION = '19.0.41';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
